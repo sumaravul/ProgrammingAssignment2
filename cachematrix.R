1ebeccd71bd9a7f9789c9ee5884fa8fa1c0e8ddb
sumaravul <sumadasu@hotmail.com>
2014-11-23 11:10:49 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,15 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## Creating a numeric vector
 makeCacheMatrix <- function(x = matrix()) {
- m<-NULL
+ m<-NULL # stores mean and is reset to null everytime matrix is called
   set<-function(y){
   x<<-y
   m<<-NULL
 }
- get<-function() x
-setmatrix<-function(solve) m<<- solve
-getmatrix<-function() m
+ get<-function() x # returns the original vector
+setmatrix<-function(solve) m<<- solve #cache the cachesolve() during the first call
+getmatrix<-function() m   # stores the cached soleve matrix
 list(set=set, get=get,
    setmatrix=setmatrix,
     getmatrix=getmatrix)
@@ -21,7 +22,7 @@ cacheSolve <- function(x=matrix(), ...) {
        return(m)
     }
      matrix<-x$get()
-    m<-solve(matrix, ...)
+    m<-solve(matrix, ...)   # calculats the inverse matrix
     x$setmatrix(m)
     m
  }
